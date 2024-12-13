725ab2fdee5f1d1e6529d7e7f3508f29bed9b7c3
NovikovDS-Insanity <Tyuiu.NovikovDS@outlook.com>
2024-12-13 17:07:37 +0300

    Taskkk6

--- Tyuiu.NovikovDS.Sprint6.Task6.V16.Lib/DataService.cs
+++ Tyuiu.NovikovDS.Sprint6.Task6.V16.Lib/DataService.cs
@@ -9,8 +9,8 @@ namespace Tyuiu.NovikovDS.Sprint6.Task6.V16.Lib
             string res = File.ReadAllText(path);
 
             var wordsWithB = res.Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries)
-                                         .Where(word => word.Contains('b', StringComparison.OrdinalIgnoreCase));
-            res = string.Join(", ", wordsWithB);
+                                         .Where(word => word.Contains('b'));
+            res = string.Join(" ", wordsWithB);
 
             return res;
         }
